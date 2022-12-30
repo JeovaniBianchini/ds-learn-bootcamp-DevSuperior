233bec52348c835b3e9457eafd9edf5eedce1832
Jeovani Bianchini <bianchini.jeovani@gmail.com>
2022-12-30 13:44:30 -0400

    Course implementation and offers with mappings

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -12,3 +12,8 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp Spring', 'https://www.publicdomainpictures.net/pictures/290000/nahled/training-course.jpg', 'https://as1.ftcdn.net/v2/jpg/03/54/26/32/1000_F_354263262_41bGumaXPIm6HBbKCJVFsDCeT3DGHhSB.jpg');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2022-11-15T03:00:00Z', TIMESTAMP WITH TIME ZONE '2023-11-15T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2022-12-15T03:00:00Z', TIMESTAMP WITH TIME ZONE '2023-12-15T03:00:00Z', 1);
