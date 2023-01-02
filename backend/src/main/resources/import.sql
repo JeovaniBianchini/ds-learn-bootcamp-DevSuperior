9708a3da7afdfc480d7ad9df587382cca5fd1e68
Jeovani Bianchini <bianchini.jeovani@gmail.com>
2023-01-02 11:32:53 -0400

    Created e seed Lesson, Content, Task

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -28,3 +28,18 @@ INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prer
 
 INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2022-11-15T13:00:00Z', null, true, false);
 INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2022-11-15T13:00:00Z', null, true, false);
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Crud 1', 1, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (1, 'abc', 'https://www.youtube.com/watch?v=0_e9Egeyk2E');
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Crud 2', 2, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (2, 'def', 'https://www.youtube.com/watch?v=0_e9Egeyk2E');
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Crud 3', 3, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (3, 'abc', 'https://www.youtube.com/watch?v=0_e9Egeyk2E');
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Tarefa crud', 4, 1);
+INSERT INTO tb_task (id, description, question_Count, approval_Count, weight, due_Date) VALUES (4, 'Tarefa para entregar', 5, 4, 1.0, TIMESTAMP WITH TIME ZONE '2022-11-25T13:00:00Z');
+
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (1, 1, 1);
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (2, 1, 1);
